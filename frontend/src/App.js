87f6440af7b71da1691a2f9ec5a2718e272a9534
Dhaval Patel <dhavalpatel32768@gmail.com>
2023-09-06 08:27:21 +0000

    added markdown editor and react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import React, { useRef } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react'
 import NotesListPage from './pages/NotesListPage';
 import EditNotePage from './pages/EditNotePage';
@@ -6,7 +8,12 @@ import EditNotePage from './pages/EditNotePage';
 function App() {
   return (
 		<ChakraProvider>
-			<EditNotePage/>
+			<Router>
+				<Switch>
+					<Route path="/" exact component={NotesListPage} />
+					<Route path="/edit" component={EditNotePage} />
+				</Switch>
+			</Router>
 		</ChakraProvider>
   );
 }
