b6ad339603414a57c38cd5a323511cb536b4b528
Dhaval Patel <dhavalpatel32768@gmail.com>
2023-09-07 01:33:55 +0000

    fix: updated router for v6

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
 import './App.css';
-import React, { useRef } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react'
 import NotesListPage from './pages/NotesListPage';
 import EditNotePage from './pages/EditNotePage';
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  { path: "/", Component: NotesListPage },
+  { path: "/edit", Component: EditNotePage },
+]);
+
+
 function App() {
   return (
 		<ChakraProvider>
-			<Router>
-				<Switch>
-					<Route path="/" exact component={NotesListPage} />
-					<Route path="/edit" component={EditNotePage} />
-				</Switch>
-			</Router>
+			<RouterProvider router={router} />;
 		</ChakraProvider>
   );
 }
