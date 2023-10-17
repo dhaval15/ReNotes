4c3aaa03235485a81c937c26e1246ce34b1bb46f
Dhaval Patel <dhavalpatel32768@gmail.com>
2023-10-17 03:09:13 -0400

    dev: (app) added QuickDialog for quick selection dialog

---
+++ app/src/components/QuickDialog.jsx
@@ -0,0 +1,175 @@
+import { Input, VStack } from "@chakra-ui/react";
+import { useEffect, useRef, useState } from "react";
+import DialogContainer from './DialogContainer';
+
+function useInView(ref) {
+	const [intersecting, setIntersecting] = useState(false)
+	const observer = new IntersectionObserver(([entry]) =>
+		setIntersecting(entry.isIntersecting)
+	)
+
+	useEffect(() => {
+		observer.observe(ref.current)
+		return () => observer.disconnect()
+	}, [ref])
+
+	return intersecting;
+}
+
+function InViewItem({ inView, children }) {
+	const topRef = useRef(null);
+	const bottomRef = useRef(null);
+
+	const inViewTop = useInView(topRef);
+	const inViewBottom = useInView(bottomRef);
+
+	useEffect(() => {
+		if (inView) {
+			if (!inViewTop) {
+				topRef.current?.scrollIntoView({
+					behavior: 'smooth',
+					block: 'start',
+				})
+			}
+			else if (!inViewBottom) {
+				bottomRef.current?.scrollIntoView({
+					behavior: 'smooth',
+					block: 'end',
+				})
+			}
+		}
+	}, [inViewTop, inViewBottom, inView]);
+
+	return (
+		<div>
+			<div aria-hidden='true' ref={topRef} />
+			{children}
+			<div aria-hidden='true' ref={bottomRef} />
+		</div>
+	)
+}
+
+function useFilter({
+	items,
+	initialQuery,
+	filter,
+}) {
+
+	const [filtered, setFiltered] = useState(items);
+	const [query, setQuery] = useState(initialQuery ?? '');
+
+
+	useEffect(() => {
+		var q = query.trim();
+		var filtered = items;
+		if (filter && q != '') {
+			filtered = filtered.filter((item) => filter(item, q));
+		}
+		setFiltered(filtered);
+	}, [filter, query, items]);
+
+	return {
+		filtered,
+		query,
+		setQuery,
+	};
+}
+
+function useQuickKeys(length, current, onSelected) {
+	const [selected, setSelected] = useState(current ?? -1);
+
+	const onKeyDown = (e) => {
+		if (e.key == 'ArrowUp' || (e.key == 'Tab' && e.shiftKey)) {
+			setSelected((prev) => prev == 0 ? 0 : prev - 1);
+			e.preventDefault();
+		}
+		else if (e.key == 'ArrowDown' || e.key == 'Tab') {
+			setSelected((prev) => prev == length - 1 ? prev : prev + 1);
+			e.preventDefault();
+		}
+		else if (e.key == 'Enter') {
+			if (selected > -1) {
+				onSelected(selected);
+			}
+			e.preventDefault();
+		}
+	};
+
+	return {
+		onKeyDown,
+		selected,
+		setSelected,
+	};
+}
+
+export default function QuickDialog({
+	isOpen,
+	onClose,
+	params,
+	dialogRef,
+	current,
+	items,
+	filter,
+	renderItem,
+	onSelected,
+	placeholder,
+}) {
+
+	const {
+		filtered,
+		query,
+		setQuery,
+	} = useFilter({ items, initialQuery: params?.query ?? '', filter });
+
+	const {
+		onKeyDown,
+		selected,
+		setSelected,
+	} = useQuickKeys(
+		filtered.length,
+		current,
+		(index) => {
+			onSelected(items[index]);
+		},);
+
+	useEffect(() => {
+		if (filtered.length > 0) {
+			setSelected(0);
+		}
+		else {
+			setSelected(-1);
+		}
+	}, [filtered]);
+
+
+	return (
+		<div
+			onKeyDown={onKeyDown}
+			ref={dialogRef}>
+			<DialogContainer
+				isOpen={isOpen}
+				onClose={() => {
+					onClose(null);
+				}}
+				title={
+					<Input
+						placeholder={placeholder ?? 'Search'}
+						value={query ?? ''}
+						onChange={(e) => setQuery(e.target.value)}
+					/>
+				}
+				body={
+					<VStack gap={0} alignItems="stretch">
+						{(filtered.map((item, index) => {
+							const isSelected = index === selected;
+							return (
+								<InViewItem inView={isSelected}>
+									{renderItem(item, index, isSelected)}
+								</InViewItem>
+							)
+						}))}
+					</VStack>
+				} />
+		</div>
+	)
+}
--- app/src/components/SelectNodeDialog.jsx
+++ app/src/components/SelectNodeDialog.jsx
@@ -1,12 +1,9 @@
-import { useState, useEffect } from 'react';
 import {
-	Input,
-	VStack,
 	Text,
 } from '@chakra-ui/react';
 import { useSelector } from 'react-redux';
-import usePromiseDisclosure, { useGetter } from '../hooks/usePromiseDisclosure';
-import DialogContainer from './DialogContainer';
+import usePromiseDisclosure from '../hooks/usePromiseDisclosure';
+import QuickDialog from './QuickDialog';
 
 export default function SelectNodeDialog({ dialogRef }) {
 	const {
@@ -15,59 +12,34 @@ export default function SelectNodeDialog({ dialogRef }) {
 		params,
 	} = usePromiseDisclosure(dialogRef);
 
-	const [query, setQuery] = useGetter((from) => from?.query, params);
-
-	const [filteredNodes, setFilteredNodes] = useState([]);
 	const nodes = useSelector((state) => state.collections.selected.nodes);
 
+	const filter = (node, query) => {
+		return node.title.toLowerCase().includes(query.toLowerCase())
+	};
 
-	useEffect(() => {
-		if (query == null || query == '') {
-			setFilteredNodes(nodes);
-		}
-		else {
-			const filtered = nodes.filter((node) =>
-				node.title.toLowerCase().includes(query.toLowerCase())
-			);
-			setFilteredNodes(filtered);
-		}
-	}, [query, nodes]);
-
+	const renderNode = (node, _, isSelected) => {
+		return (<Text padding={2}
+			key={node.id}
+			width="100%"
+			textAlign="left"
+			bg={isSelected ? "gray.100" : "transparent"}
+		>
+			{node.title}
+		</Text>)
+	};
 	return (
-		<div ref={dialogRef}>
-			<DialogContainer
-				isOpen={isOpen}
-				onClose={() => {
-					onClose(null);
-				}}
-				title="Select a Node"
-				body={
-					<>
-						<Input
-							placeholder="Search for a node"
-							value={query ?? ''}
-							onChange={(e) => setQuery(e.target.value)}
-						/>
-						<VStack align="start" spacing={2} mt={4}>
-							{filteredNodes.length === 0 ? (
-								<Text>No nodes found.</Text>
-							) : (
-								filteredNodes.map((node) => (
-									<Text
-										key={node.id}
-										onClick={() => {
-											onClose(node);
-										}}
-										width="100%"
-										textAlign="left"
-									>
-										{node.title}
-									</Text>
-								))
-							)}
-						</VStack>
-					</>
-				} />
-		</div>
+		<QuickDialog
+			isOpen={isOpen}
+			onClose={onClose}
+			params={params}
+			dialogRef={dialogRef}
+			items={nodes}
+			placeholder="Search for a node"
+			renderItem={renderNode}
+			filter={filter}
+			onSelected={(node) => {
+				onClose(node);
+			}} />
 	);
 };
